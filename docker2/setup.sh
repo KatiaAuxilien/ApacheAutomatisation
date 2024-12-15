bcb93678e8612a89175555ae5fb3a9f5a87f38b0
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-12-15 23:26:22 +0100

    Update setup.sh

--- docker2/setup.sh
+++ docker2/setup.sh
@@ -615,10 +615,10 @@ SecStatusEngine Off" > /apache2/modsecurity.conf
     logs_info "Sécurisation du .htaccess ..."
         
         touch /www/.htpasswd
-        error_handler $? "La création du fichier /var/www/.htpasswd a échouée."
+        error_handler $? "La création du fichier /www/.htpasswd a échouée."
 
         htpasswd -cb /www/.htpasswd admin \${HTACCESS_PASSWORD}
-        error_handler $? "L'écriture dans le fichier /var/www/.htpasswd a échouée."
+        error_handler $? "L'écriture dans le fichier /www/.htpasswd a échouée."
 
 #======================================================================#
 # Création et configuration de n sites-enabled                         #
