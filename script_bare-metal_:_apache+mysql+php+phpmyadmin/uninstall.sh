b11e6a0f90f6baa222a76e2f3e503adce543b007
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-12-17 15:56:57 +0100

    Create uninstall.sh

---
+++ script_bare-metal_:_apache+mysql+php+phpmyadmin/uninstall.sh
@@ -0,0 +1,25 @@
+#TODO : Messages de logs
+#TODO : Vérification du lancement en droits admin
+
+#TODO : Arrêt de tout les services
+
+#TODO : Suppression des fichiers de configuration de Apache
+#TODO : Suppression des fichiers de configuration HTTPS
+#TODO : Suppression des fichiers de configuration de ModSecurity
+#TODO : Suppression des fichiers de configuration  de ModEvasive
+#TODO : Suppression des fichiers de configuration de ModRatelimit
+#TODO : Suppression de la configuration des deux sites (siteA, siteB)
+#TODO : Suppression de la configuration de la page confidentielle (.htaccess et .htpasswd)
+#TODO : Désinstallation de Apache
+
+#TODO : Désinstallation PHP
+#TODO : Suppression des fichiers de configuration de PHP
+
+#TODO : Désinstallation mysql
+#TODO : Suppression des fichiers de configuration de mysql
+
+#TODO : Désinstallation PhpMyAdmin
+#TODO : Suppression des fichiers de configuration de PhpMyAdmin
+
+
+#TODO : Suppression de la base de données d'intro
