9ac37e854e1e8be57324e8d9214ac3ffb1416e45
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-12-17 15:56:24 +0100

    Create install.sh

---
+++ script_bare-metal_:_apache+mysql+php+phpmyadmin/install.sh
@@ -0,0 +1,27 @@
+#TODO : Messages de logs
+#TODO : Vérification du lancement en droits admin
+#TODO : Vérification des variables fournies dans le .env
+
+#TODO : Installation de Apache
+#TODO : Configuration de Apache
+
+#TODO : Installation PHP
+#TODO : Configuration de PHP
+
+#TODO : Installation mysql
+#TODO : Configuration de mysql
+
+#TODO : Installation PhpMyAdmin
+#TODO : Configuration de PhpMyAdmin
+
+#TODO : Faire fonctionner les 4 services ensemble.
+
+#TODO : Création de deux sites (siteA, siteB)
+#TODO : Créer une page confidentielle (.htaccess et .htpasswd)
+
+#TODO : Créer une base de données d'intro
+
+#TODO : Sécurisation du serveur web et des sites par HTTPS
+#TODO : Sécurisation - Installation et configuration de ModSecurity
+#TODO : Sécurisation - Installation et configuration de ModEvasive
+#TODO : Sécurisation - Installation et configuration de ModRatelimit
